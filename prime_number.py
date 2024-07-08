5c11ce3ad39c516c073b7c84c47b760895b41c39
Marelyn Deschamps <78377414+Marelyn15@users.noreply.github.com>
2024-07-08 01:05:45 -0700

    Initial commit

---
+++ prime_number.py
@@ -0,0 +1,16 @@
+def prime_checker(number):
+    result = []
+    for i in range(2,101):
+        result.append(number % i)
+    
+    #print(result)
+    number_zeros = result.count(0)
+
+    if number_zeros > 1 or number == 1:
+        print("It's not a prime")
+    else:
+        print("It's a prime")
+
+#Do NOT change any of the code below👇
+n = int(input()) # Check this number
+prime_checker(number=n)
